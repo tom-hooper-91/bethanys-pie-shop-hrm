ab589ada708b5886407cec1221cc969cbd32046d
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-18 10:35:54 +1200

    05 - methods

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -7,56 +7,23 @@ namespace BethanysPieShopHRM
         static void Main(string[] args)
         {
             int monthlyWage = 1234;
-            int months = 12, bonus = 1000;
+            int monthsWorked = 12;
 
-            bool isActive = true;
-            double rating = 99.25;
+            int yearlyWage = CalculateYearlyWage(monthlyWage, monthsWorked);
 
-            double ratePerHour = 12.34;
-            int numberOfHourWorked = 165;
-
-            double currentMonthWage = ratePerHour * numberOfHourWorked + bonus;
-
-            Console.WriteLine(currentMonthWage);
-
-            ratePerHour += 3;
-
-            if (currentMonthWage > 2000)
-            {
-                Console.WriteLine("Top paid employee");
-            }
-
-            int numberOfEmployees = 15;
-            numberOfEmployees--;
-
-            int intMaxValue = int.MaxValue;
-            int intMinValue = int.MinValue;
-
-            char userSelection = 'a';
-            char upperVersion = char.ToUpper(userSelection);
-            bool isDigit = char.IsDigit(userSelection);
-            bool isLetter = char.IsLetter(userSelection);
-
-            DateTime hireDate = new DateTime(2021, 3, 28, 14, 30, 0);
-
-            Console.WriteLine(hireDate);
-
-            // DateTime exitDate = new DateTime(2021, 15, 11);
-
-            DateTime startDate = hireDate.AddDays(15);
-
-            DateTime currentDate = DateTime.Now;
-            bool areWeInDst = currentDate.IsDaylightSavingTime();
-
-            DateTime startHour = DateTime.Now;
-            TimeSpan workTime = new TimeSpan(8, 35, 0);
-            DateTime endHour = startHour.Add(workTime);
-
-            Console.WriteLine(startHour.ToLongDateString());
-            Console.WriteLine(endHour.ToShortTimeString());
-
+            Console.WriteLine($"Yearly Wage is : ${yearlyWage}");
 
             Console.ReadLine();
+        }
+
+        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+        {
+            if (numberOfMonthsWorked == 12)
+            {
+                return monthlyWage * (numberOfMonthsWorked + 1); // Add's a bonus month if they have worked all year
+            }
+
+            return monthlyWage * numberOfMonthsWorked;
         }
     }
 }
