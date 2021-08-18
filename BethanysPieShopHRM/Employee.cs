654be1391a92701c98734c741a6f97bf0234e5f6
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-19 10:54:44 +1200

    refactor methods to contain properties

--- BethanysPieShopHRM/Employee.cs
+++ BethanysPieShopHRM/Employee.cs
@@ -72,7 +72,14 @@ namespace BethanysPieShopHRM
             }
             set
             {
-                wage = value;
+                if (value < 0)// validation logic for properties
+                {
+                    wage = 0;
+                }
+                else
+                {
+                    wage = value;
+                }
             }
         }
 
@@ -115,12 +122,12 @@ namespace BethanysPieShopHRM
         // Constructors
         public Employee(string first, string last, string em, DateTime bd, EmployeeType empType, double rate)
         {
-            firstName = first;
-            lastName = last;
-            email = em;
-            birthDay = bd;
-            employeeType = empType;
-            hourlyRate = rate;
+            FirstName = first;
+            LastName = last;
+            Email = em;
+            BirthDay = bd;
+            EmployeeType = empType;
+            HourlyRate = rate;
         }
 
         public Employee(string first, string last, string em, DateTime bd, EmployeeType empType) : this(first, last, em, bd, empType, 0) // calls the first constructor using 'this'
@@ -130,28 +137,28 @@ namespace BethanysPieShopHRM
         // Methods
         public void PerformWork()
         {
-            numberOfHoursWorked++;
+            NumberOfHoursWorked++;
 
-            Console.WriteLine($"{firstName} {lastName} is now working!");
+            Console.WriteLine($"{FirstName} {LastName} is now working!");
         }
 
         public void StopWorking()
         {
-            Console.WriteLine($"{firstName} {lastName} has stopped working");
+            Console.WriteLine($"{FirstName} {LastName} has stopped working");
         }
 
         public double RecieveWage()
         {
-            wage = numberOfHoursWorked * hourlyRate;
-            Console.WriteLine($"The wage for {numberOfHoursWorked} is {wage}.");
-            numberOfHoursWorked = 0;
+            Wage = NumberOfHoursWorked * HourlyRate;
+            Console.WriteLine($"The wage for {NumberOfHoursWorked} is {Wage}.");
+            NumberOfHoursWorked = 0;
 
-            return wage;
+            return Wage;
         }
 
         public void DisplayEmployeeDetails()
         {
-            Console.WriteLine($"\nFirst Name: {firstName}\nLast Name: {lastName}\nEmail: {email}\nBirthday: {birthDay}\nEmployee Type: {employeeType}\n");
+            Console.WriteLine($"\nFirst Name: {FirstName}\nLast Name: {LastName}\nEmail: {Email}\nBirthday: {BirthDay}\nEmployee Type: {EmployeeType}\n");
         }
 
     }
