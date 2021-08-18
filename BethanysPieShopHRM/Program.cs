f83a7083ad930e28dec76da5c3dc3fd7cebf248e
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-19 10:24:59 +1200

    create second Employee object

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -11,12 +11,24 @@ namespace BethanysPieShopHRM
 
             Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), EmployeeType.Manager, 25);
 
+            Employee george = new Employee("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), EmployeeType.Research, 30);
+
+            bethany.firstName = "John";
+            bethany.hourlyRate = 15; // Objects can be altered after creation using dot notation
+
             bethany.DisplayEmployeeDetails();
             bethany.PerformWork();
             bethany.PerformWork();
             bethany.PerformWork();
             bethany.PerformWork();
             bethany.RecieveWage();
+
+            george.DisplayEmployeeDetails();
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork();
+            george.RecieveWage();
 
             Console.ReadLine();
         }
