e061104254b9b5441b058701bc5f1911621839f3
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-19 09:47:59 +1200

    create Employee class

---
+++ BethanysPieShopHRM/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace BethanysPieShopHRM
+{
+    public class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+        public DateTime birthDay;
+
+        public EmployeeType employeeType;
+
+        public void PerformWork()
+        {
+            numberOfHoursWorked++;
+
+            Console.WriteLine($"{firstName} {lastName} is now working!");
+        }
+
+        public void StopWorking()
+        {
+            Console.WriteLine($"{firstName} {lastName} has stopped working");
+        }
+
+        public double RecieveWage()
+        {
+            wage = numberOfHoursWorked * hourlyRate;
+            Console.WriteLine($"The wage for {numberOfHoursWorked} is {wage}.");
+            numberOfHoursWorked = 0;
+
+            return wage;
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst Name: {firstName}\nLast Name: {lastName}\nEmail: {email}\nBirthday: {birthDay}\nEmployee Type: {employeeType}\n");
+        }
+
+
+
+
+        public Employee()
+        {
+        }
+    }
+}
---
+++ BethanysPieShopHRM/EmployeeType.cs
@@ -0,0 +1,10 @@
+﻿namespace BethanysPieShopHRM
+{
+    public enum EmployeeType
+    {
+        Sales,// 0
+        Manager,// 1
+        Research,// 2
+        StoreManager// 3
+    }
+}
