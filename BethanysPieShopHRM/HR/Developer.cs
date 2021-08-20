7f1436a3bbe7778d8914bacbf1629dc9b27a967a
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-20 12:17:04 +1200

    make Developer sealed class and add notes

--- BethanysPieShopHRM/HR/Developer.cs
+++ BethanysPieShopHRM/HR/Developer.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 namespace BethanysPieShopHRM.HR
 {
-    public class Developer : Employee
+    public sealed class Developer : Employee// sealed keyword stops any class from deriving from Developer
     {
         public Developer(string first, string last, string em, DateTime bd, double? rate) : base(first, last, em, bd, rate)
         {
         }
     }
+
+    //public class JuniorDeveloper : Developer ----- this is not possible due to use of sealed keywords
+    //{
+
+    //}
 }
