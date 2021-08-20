8889b70a2845cb548ae4c081c0839c460be86706
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-20 15:46:51 +1200

    add IEmployee interface

---
+++ BethanysPieShopHRM/HR/IEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BethanysPieShopHRM.HR
+{
+    public interface IEmployee// interface is similar to abstract class but can have multiple inheritance
+    {
+        double ReceiveWage();
+
+        void GiveBonus();
+
+        void PerformWork();
+
+        void StopWorking();
+
+        void DisplayEmployeeDetails();
+    }
+}
